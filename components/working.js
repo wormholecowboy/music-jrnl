a378c95e3417137da3262e3db4a96b013f70e298
wormholecowboy <wormholecowboy@gmail.com>
2022-11-28 10:28:49 -0500

    add drag and drop

--- components/working.js
+++ components/working.js
@@ -1,26 +1,36 @@
-import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 export default function WorkingArea() {
+  let phrases = [
+    {pname:"blue", id:"blue"},
+    {pname:"yellow", id:"yellow"},
+    {pname:"green", id:"green"},
+    {pname:"red", id:"red"},
+  ]
   return (
     <div className="flex flex-row gap-5">
       <DragDropContext>
-        <ul>
-          <li key={1} value="one">
-            one
-          </li>
-          <li key={2} value="two">
-            two
-          </li>
-          <li key={3} value="three">
-            three
-          </li>
-          <li key={4} value="four">
-            four
-          </li>
-          <li key={5} value="five">
-            five
-          </li>
-        </ul>
+        <Droppable droppableId="phrases">
+          {(provided) => (
+            <ul {...provided.droppableProps} ref={provided.innerRef}>
+              {phrases.map(({pname, id}, index) => {
+    return (
+              <Draggable key={id} draggableId={id} index={index}>
+                {(provided) => (
+                  <li
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    ref={provided.innerRef}
+                  >
+                    <span>{pname}</span>
+                  </li>
+                )}
+              </Draggable>
+             );
+             })}          
+            </ul>
+          )}
+        </Droppable>
       </DragDropContext>
     </div>
   );
