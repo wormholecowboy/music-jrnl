db64ee63217bba5384b229343e712fbf55ac3ddc
wormholecowboy <wormholecowboy@gmail.com>
2023-11-27 22:27:30 -0500

    added drag handles to work pills

--- components/work-draggablePhrase.js
+++ components/work-draggablePhrase.js
@@ -1,6 +1,8 @@
+import Image from 'next/image';
 import { useSortable } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
 import { Card } from "@mui/material";
+import { list } from 'postcss';
 
 export default function DraggablePhrase(props) {
 
@@ -15,8 +17,18 @@ export default function DraggablePhrase(props) {
         transition
     }
     return (
-        <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
-            <Card className="mx-2">{props.children}</Card>
+        <div ref={setNodeRef} style={style} >
+            <Card className="mx-2">{props.children}
+                <span {...attributes} {...listeners}>
+                    <Image
+                        alt="move"
+                        src="/../public/move.png"
+                        className="rounded-full m-2"
+                        width={20}
+                        height={20}
+                    />
+                </span>
+            </Card>
         </div>
     )
 }
---
+++ dev-notes.js
@@ -0,0 +1,7 @@
+/*
+    *  TODO: add random colors for pills
+    *  TODO: add random names for phrases
+    *  TODO: increase bpm and add more scales
+    *
+    *
+    */
