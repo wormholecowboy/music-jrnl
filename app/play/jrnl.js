3b6d61eb3bdb386917cdb3c6c9b38f90a6d3c9e4
wormhole cowboy <wormholecowboy@gmail.com>
2024-08-14 21:18:36 -0400

    added icon buttons to jrnl

--- app/play/jrnl.js
+++ app/play/jrnl.js
@@ -1,7 +1,15 @@
 "use client";
-import { List, ListItem, ListItemButton } from "@mui/material";
+import {
+  Icon,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
 import { useState, useEffect } from "react";
 import getPhrases from "../actions/get-phrases";
+import Image from "next/image";
 
 export default function Jrnl() {
   const [selected, setSelected] = useState();
@@ -24,24 +32,30 @@ export default function Jrnl() {
 
   return (
     <>
-      <div className="flex flex-row justify-center my-3 gap-2">
-        <button className="self-center px-4 py-2 text-color4 border-2 border-color4 shadow-lg rounded-lg bg-color5">
-          Load
-        </button>
-        <button className="self-center px-4 py-2 text-color4 border-2 border-color4 shadow-lg rounded-lg bg-color5">
-          Delete
-        </button>
-        <button className="self-center px-4 py-2 text-color4 border-2 border-color4 shadow-lg rounded-lg bg-color5">
-          Rename
-        </button>
+      <div className="flex flex-col">
+        <p>
+          <strong>Your Phrase Journal</strong>
+        </p>
+        <div></div>
+        <List>
+          {phrases.map((phraseObj) => (
+            <ListItem key={phraseObj.phrase_id}>
+              <ListItemText>{phraseObj.name}</ListItemText>
+              <ListItemIcon>
+                <span>
+                  <Image src="/rename.png" width={20} height={20} />
+                </span>
+                <span>
+                  <Image src="/larrow.png" width={20} height={20} />
+                </span>
+                <span>
+                  <Image src="/trash.png" width={20} height={20} />
+                </span>
+              </ListItemIcon>
+            </ListItem>
+          ))}
+        </List>
       </div>
-      <List>
-        {phrases.map((phraseObj) => (
-          <ListItemButton key={phraseObj.phrase_id}>
-            {phraseObj.name}
-          </ListItemButton>
-        ))}
-      </List>
     </>
   );
 }
